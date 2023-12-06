b8329551a98e64fa7c17702539291ca48356f4e1
Henrique Buzin <cultura.henrique@gmail.com>
2023-12-06 20:55:23 -0300

    Update start.py

--- teste_a/medidor/src/start.py
+++ teste_a/medidor/src/start.py
@@ -1,9 +1,15 @@
 import docker
 import threading
+import time
+
+stop_threads = False  # Flag to signal threads to stop
 
 def monitor_container_logs(container, container_number):
+    global stop_threads
     for line in container.logs(stream=True):
-        print(f"Contêiner {container_number}: {line.decode('utf-8').strip()}\n")
+        if stop_threads:
+            break
+        print(f"Container {container_number}: {line.decode('utf-8').strip()}\n")
 
 def create_replicas(num_replicas):
     client = docker.from_env()
@@ -11,11 +17,12 @@ def create_replicas(num_replicas):
 
     for i in range(1, num_replicas + 1):
         try:
+            container_name = f"medidor-{i}"
             environment = {"REPLICANUMBER": str(i)}
-            container = client.containers.run("medidor", detach=True, environment=environment)
+            container = client.containers.run("medidor", name=container_name, detach=True, environment=environment)
             containers.append(container)
         except Exception as e:
-            print(f"Erro ao iniciar o contêiner: {e}")
+            print(f"Error starting container: {e}")
 
     threads = []
     for i, container in enumerate(containers, start=1):
@@ -23,25 +30,27 @@ def create_replicas(num_replicas):
         thread.start()
         threads.append(thread)
 
-    try:
-        for thread in threads:
-            thread.join()
-    except KeyboardInterrupt:
-        print("Interrompido pelo usuário, parando contêineres...\n")
-        for container in containers:
-            try:
-                container.stop()
-            except Exception as e:
-                print(f"Erro ao parar o contêiner: {e}")
+    return threads, containers
 
+def stop_and_remove_containers(containers):
+    global stop_threads
+    stop_threads = True  # Signal threads to stop
+    print("Interrupted by user, stopping and removing containers...\n")
     for container in containers:
         try:
-            container.wait()
+            container.stop()
+            container.remove()  # Remove the container after stopping
+            print(f"Container removed: {container.name}")
         except Exception as e:
-            print(f"Erro ao esperar o contêiner finalizar: {e}")
-
-    print("Todos os contêineres foram parados.")
+            print(f"Error stopping/removing container: {e}")
+    print("All containers have been stopped and removed.")
 
 if __name__ == "__main__":
-    num_replicas = int(input("Informe o número de réplicas desejado: "))
-    create_replicas(num_replicas)
+    try:
+        num_replicas = int(input("Enter the desired number of replicas: "))
+        threads, containers = create_replicas(num_replicas)
+
+        while True:
+            time.sleep(0.1)  # Prevent the main thread from blocking
+    except KeyboardInterrupt:
+        stop_and_remove_containers(containers)
