055195240632b9518d9e77dde13844a7f917bba8
Henrique Buzin <cultura.henrique@gmail.com>
2024-03-06 06:08:53 -0300

    FIX WINDOOWS

--- teste_c/processing/main.py
+++ teste_c/processing/main.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("\Camada de Processamento encerrando...")
+        print("\nCamada de Processamento encerrando...")
     except Exception as e:
         print(f"\nErro durante a execução da Camada de Processamento: {e}")
     finally:
