3a63663bedde2e4b4d45818da433ab9be74cbcda
Henrique Buzin <cultura.henrique@gmail.com>
2024-05-29 01:18:27 -0300

    OK

--- energy_fog_local/medidor_http/main.py
+++ energy_fog_local/medidor_http/main.py
@@ -1,8 +1,14 @@
-import os
 import requests
 import time
 import csv
 import argparse
+
+STREETS = [
+    "1st Street", "2nd Street", "3rd Street", "4th Street", "5th Street",
+    "6th Street", "7th Street", "8th Street", "9th Street", "10th Street",
+    "11th Street", "12th Street", "13th Street", "14th Street", "15th Street",
+    "16th Street", "17th Street", "18th Street", "19th Street", "20th Street"
+]
 
 def read_csv(file_name):
     with open(file_name, newline='') as csvfile:
@@ -25,9 +31,11 @@ def send_data(data, url):
         time.sleep(1)
 
 def start_sending_data(file_name, url, instance_id):
+    street = STREETS[(instance_id - 1) % len(STREETS)]
     for index, data in enumerate(read_csv(file_name), start=1):
         data['type'] = 'consumption'
         data['id'] = instance_id
+        data['street'] = street
         send_data(data, url)
         time.sleep(1)
 
