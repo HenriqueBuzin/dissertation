be2bab1cb68d0b3227b385244cf7c9353f09c1c1
Henrique Buzin <cultura.henrique@gmail.com>
2024-06-01 12:50:34 -0300

    Update http_sender.py

--- fog/local/energy/service/http_sender.py
+++ fog/local/energy/service/http_sender.py
@@ -15,27 +15,31 @@ async def send_file_and_data_http(file_path, sftp_host, sftp_port, sftp_username
                 content = await f.read()
                 content_base64 = base64.b64encode(content).decode('utf-8')
 
-                data = {
-                    "file": content_base64,
-                    "type": "ftp",
-                    "sftp_host": sftp_host,
-                    "sftp_port": sftp_port,
-                    "sftp_username": sftp_username,
-                    "sftp_password": sftp_password,
-                    "remote_path": remote_path
-                }
+            data = {
+                "file": content_base64,
+                "type": "ftp",
+                "sftp_host": sftp_host,
+                "sftp_port": sftp_port,
+                "sftp_username": sftp_username,
+                "sftp_password": sftp_password,
+                "remote_path": remote_path
+            }
             
-                print(data)
-                async with aiohttp.ClientSession() as session:
-                    async with session.post(url, json=data) as response:
-                        if response.status == 200:
-                            print(f"Status da resposta: {response.status}")
-                            print(f"Arquivo {file_path} enviado com sucesso.")
+            print(data)
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, json=data) as response:
+                    if response.status == 200:
+                        print(f"Status da resposta: {response.status}")
+                        print(f"Arquivo {file_path} enviado com sucesso.")
+                        await asyncio.sleep(1)
+                        try:
                             os.remove(file_path)
                             print(f"Arquivo {file_path} removido.")
-                            return
-                        else:
-                            print(f"Erro ao enviar. Status da resposta: {response.status}. Tentando novamente em {delay} segundos.")
+                        except Exception as e:
+                            print(f"Erro ao remover o arquivo: {str(e)}")
+                        return
+                    else:
+                        print(f"Erro ao enviar. Status da resposta: {response.status}. Tentando novamente em {delay} segundos.")
         except Exception as e:
             print(f"Erro ao enviar os dados: {str(e)}")
 
