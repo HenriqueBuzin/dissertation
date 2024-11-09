7f8f2ab0afbe5b5b8ca8466bdab55a5431becbdf
Henrique Buzin <cultura.henrique@gmail.com>
2024-11-09 12:37:12 -0300

    Update app.py

--- manager/src/app.py
+++ manager/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 import unicodedata
 import socket
