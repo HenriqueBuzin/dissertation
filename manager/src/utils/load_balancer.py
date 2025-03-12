e9213e5bb53a9447c35c03ec7441708894ea0fce
Henrique Buzin <cultura.henrique@gmail.com>
2025-03-12 16:29:08 -0300

    Update load_balancer.py

--- manager/src/utils/load_balancer.py
+++ manager/src/utils/load_balancer.py
@@ -1,6 +1,6 @@
 # utils/load_balancer.py
 
-from .network import get_available_port, create_or_get_bairro_network
+from .network import get_available_port, create_or_get_bairro_network, create_or_get_lb_network
 from .general import normalize_container_name
 from .docker_utils import get_docker_client, get_docker_errors
 
@@ -29,7 +29,8 @@ def create_load_balancer(bairro, container_name, image, container_types):
         http_port = get_available_port()
         coap_port = get_available_port(http_port + 1)
 
-        network_name = create_or_get_bairro_network(bairro)
+        bairro_network = create_or_get_bairro_network(bairro)
+        lb_network = create_or_get_lb_network()
 
         full_container_name = f"{normalize_container_name(bairro)}_{container_name}_1"
 
@@ -37,7 +38,7 @@ def create_load_balancer(bairro, container_name, image, container_types):
             image,
             name=full_container_name,
             detach=True,
-            network=network_name,
+            network=None,
             environment={
                 "LOAD_BALANCER_HTTP_PORT": str(http_port),
                 "LOAD_BALANCER_COAP_PORT": str(coap_port),
@@ -48,6 +49,10 @@ def create_load_balancer(bairro, container_name, image, container_types):
             },
             labels={"type": str(container_types["load_balancer"]["id"])},
         )
+
+        client.networks.get(bairro_network).connect(full_container_name)
+        client.networks.get(lb_network).connect(full_container_name)
+
         print(f"Load Balancer criado com sucesso. HTTP: {http_port}, CoAP: {coap_port}")
         return http_port, coap_port
 
