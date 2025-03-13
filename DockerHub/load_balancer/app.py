ef7e804f9ae03d9848ba018c6dbc5b470296e9c0
Henrique Buzin <cultura.henrique@gmail.com>
2025-03-13 00:00:07 -0300

    Update app.py

--- DockerHub/load_balancer/app.py
+++ DockerHub/load_balancer/app.py
@@ -1,27 +1,21 @@
 # app.py
 
-import asyncio
 from http_server import start_http_server
 from coap_server import start_coap_server
+from registry import get_available_nodes
+import asyncio
 import logging
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    http_port = 5000  # HTTP Port
-    coap_port = 5683  # CoAP Port
+    http_port = 5000
+    coap_port = 5683
 
     logging.info("Starting HTTP and CoAP servers...")
 
-    # Initialize the structure to track available nodes
-    available_nodes = {
-        "consumption_m3_per_hour": [],
-        "consumption_kwh_per_hour": [],
-        # Adicione outros tipos específicos conforme necessário
-    }
+    available_nodes = get_available_nodes()
 
-    # Start both servers concurrently, passing available_nodes
     await asyncio.gather(
         start_http_server(http_port, available_nodes),
         start_coap_server(coap_port, available_nodes),
